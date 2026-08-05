dd0c72d1d1ee540ca048e7263edcfa1e00f84c89
julian.huerfano <julian.huerfano@sofka.com.co>
2026-08-05 09:19:53 -0500

    (NEW) primer commit en Azure

--- src/main/java/ms_onboarding_data_store_api/data/adapter/s3/S3BiometricStorageAdapter.java
+++ src/main/java/ms_onboarding_data_store_api/data/adapter/s3/S3BiometricStorageAdapter.java
@@ -1,6 +1,6 @@
-package ms_onboarding_data_store_api.data.adapter.s3;
+/*package ms_onboarding_data_store_api.data.adapter.s3;
 
 import ms_onboarding_data_store_api.application.port.out.BiometricStoragePort;
 
 public class S3BiometricStorageAdapter implements BiometricStoragePort {
-}
+}*/
